b49b847bf07014913ceb71c8a5640f6f6834b541
renoaja <adrianofemaz2004@gmail.com>
2025-11-29 22:04:24 +0700

    install tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://api.capstone.yapp.web.id",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
-    }),
-  ],
-})
+    },
+  },
+});
