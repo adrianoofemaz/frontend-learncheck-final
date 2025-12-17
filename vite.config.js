987b576c0148a3d59db42806e59011e63e989f40
Muhammad Fajar Sholehuddin Maulana Putra <152793334+sholehuddinn@users.noreply.github.com>
2025-12-17 21:02:07 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true,
-    port: 5173,
-    strictPort: true,
-    allowedHosts: [
-      "capstone.web.id",
-      "www.capstone.web.id",
-    ],
     proxy: {
       "/api": {
         target: "https://api.capstone.web.id",
